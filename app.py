b69725e22129dcc5348e814592efa721d35395ff
rajkumar <rajkumar2003sin@gmail.com>
2025-06-19 18:08:59 +0530

    Add working pytest sample

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-import numpy as np
-import pandas as pd
-from flask import flask, request, jsonify
-from sklearn.linear_model import LinearRegression
-import joblib
-import os
-import logging
+# import numpy as np
+# import pandas as pd
+# from flask import flask, request, jsonify
+# from sklearn.linear_model import LinearRegression
+# import joblib
+# import os
+# import logging
+def add(a, b):
+    return a + b
 
+def test_add():
+    assert add(2, 3) == 5
